0d8a5531f647ba2c5a8cd18d93eeef22a7776388
gbelecedric <cedric.gbele@uvci.edu.ci>
2019-11-16 17:46:33 +0000

    compilation ok

--- projetvideo/editor/views.py
+++ projetvideo/editor/views.py
@@ -12,28 +12,56 @@ def editor(request):
 
 
 def postCode(request):
-    url = 'https://tpcg.tutorialspoint.com/tpcg.php'
+    # url pour avoir le token
+    base_url_post = 'https://api.judge0.com//submissions?base64_encoded=false&wait=false'
+
+ 
+    # url = 'https://tpcg.tutorialspoint.com/tpcg.php'
     cod = request.POST.get('contentA')
     print('code----------' ,cod)
 
     
     
     
+    # data = {
+    #     'lang': 'python',
+    #     'device': '',
+    #     'code': cod,
+    #     'stdinput': '',
+    #     'ext': 'py',
+    #     'compile': '0',
+    #     'execute': 'python main.py',
+    #     'mainfile': 'main.py',
+    #     'uid': '5116890',
+    # }
+    # https://api.judge0.com//submissions/9f046ab5-cda4-4dda-92e7-25878eab11c5?base64_encoded=false&wait=false
     data = {
-        'lang': 'python',
-        'device': '',
+        'source_code': cod,
+        'language_id': 35,
         'code': cod,
-        'stdinput': '',
-        'ext': 'py',
-        'compile': '0',
-        'execute': 'python main.py',
-        'mainfile': 'main.py',
-        'uid': '5116890',
+        'expected_output': '6',
     }
-    req = requests.post(url, data = data)
-    resultat = req.text
+    req = requests.post(base_url_post, data = data)
+    token = req.text
+    data = json.loads(token)
+    token= data['token']
+    print(token)
+    base_url_get = 'https://api.judge0.com/submissions/{}?base64_encoded=false'.format(token)
+    reqapi=requests.get(base_url_get)
+    resultat = reqapi.text
+    datapi = json.loads(resultat)
+
+    while datapi['status']['id'] == 1 or datapi['status']['id'] == 2 :
+
+        reqapi=requests.get(base_url_get)
+
+    if datapi['status']['id'] == 3:
+        resultat = "Resultat Valide"
+    else:
+        resultat=datapi['status']['description']
+    
     print(resultat)
-    # print(req.status_code)
+    # print(reqapi.status_code)
     # print(req.text)
     datas = {
         'succes':True,
